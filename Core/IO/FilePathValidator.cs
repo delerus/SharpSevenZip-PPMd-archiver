8c0cfa74a42967c54b41e2014acda28ed1482083
delerus <delerus21@gmail.com>
2025-05-25 17:30:43 +0300

    added comments to FilePathValidator.cs

--- Core/IO/FilePathValidator.cs
+++ Core/IO/FilePathValidator.cs
@@ -3,8 +3,15 @@ using System.IO;
 
 namespace SevenZipSharpArchiver.Core.IO
 {
+    /// <summary>
+    /// Validates file paths for reading and writing operations
+    /// </summary>
     public static class FilePathValidator
     {
+        /// <summary>
+        /// Validates the path to a file that will be read
+        /// </summary>
+        /// <param name="filePath">The path to the file to validate</param>
         public static void ValidateReadFile(string filePath)
         {
             if (string.IsNullOrWhiteSpace(filePath))
@@ -23,6 +30,10 @@ namespace SevenZipSharpArchiver.Core.IO
             }
         }
 
+        /// <summary>
+        /// Validates the path to a file that will be written
+        /// </summary>
+        /// <param name="filePath">The path to the file to validate</param>
         public static void ValidateWriteFile(string filePath)
         {
             if (string.IsNullOrWhiteSpace(filePath))
